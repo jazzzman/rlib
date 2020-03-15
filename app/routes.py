8507d1c470ff7655e00b53c089c0108603a65c41
jazzzman <peiyot@gmail.com>
2020-03-16 00:05:22 +0300

    events UI, json post with ajax

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,15 @@ from app.forms import LoginForm
 from app.models import User, Publication, Journal
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET','POST'])
 @login_required
 def index():
+    if request.method == 'POST':
+        filters = request.get_json();
+        print(filters)
+    page = request.args.get('page', 1, type=int)
+    authors = request.args.get('page', 1, type=int)
+    ptub_type = request.args.get('page', 1, type=int)
     page = request.args.get('page', 1, type=int)
     publications = Publication.query.order_by(Publication.year.desc()).paginate(
             page, app.config['PUBLICATIONS_PER_PAGE'], False)
@@ -18,10 +24,10 @@ def index():
     max_nav_btn = min(publications.total//publications.per_page+1,page+3)+1
     nav_btns = [(url_for('index',page=i),i) for i in range(max(1,page-2),max_nav_btn)]
     journals = Journal.query.all()
-    years = Journal.query.all()
+    years = sorted(set(Publication.query.values(Publication.year)))
     return render_template('index.html', title='RLib', publications = publications.items,
                             next_url=next_url, prev_url=prev_url, nav_btns=nav_btns,
-                            journals=journals)
+                            journals=journals, years=years)
 
 @app.route('/signin', methods=['GET','POST'])
 def signin():
---
+++ app/static/js/index.page.js
@@ -0,0 +1,74 @@
+var filter = {};
+// Setting dataTable
+$('#dt-basic-checkbox').dataTable({
+    searching: false,
+    paging: false,
+    columnDefs: [{
+        orderable: false,
+        className: 'select-checkbox',
+        targets: 0
+    }],
+    select: {
+        style: 'os',
+        selector: 'td:first-child'
+    }
+});
+$('#dt-basic-checkbox_length').insertBefore('#dt-basic-checkbox_paginate');
+$('#dt-basic-checkbox_length').addClass('dataTables_paginate float-left');
+//TODO implement ajax paging
+var page = 1;
+if (/page=(\d*)/.test($(location).attr('href'))){
+    page = /page=(\d*)/.exec($(location).attr('href'));
+}
+$("li.page-item:contains("+page+")").addClass('active');
+$(function (){
+    $("[id^='drop-']").click(function(){
+        var inputId = this.id.replace("drop","input");
+        var key = this.id.replace("drop-","");
+        //deselect selects
+        $("#"+inputId)[0].selectedIndex = 0;
+        $("#"+inputId).val("");
+        if (key in filter){
+            delete filter[key];
+        }
+        deactivateBtnClass(this.id);
+        sendFilters();
+    });
+    // Filters
+    $(".custom-select").change(function(){
+        var selectedValues = $("option:selected", this).map(function(){
+            return this.value;
+        })
+        .toArray();
+        var id = this.id.replace("input","drop");
+        activateBtnClass(id);
+        var key = this.id.replace("input-","");
+        filter[key] = selectedValues;
+        console.log(filter);
+        sendFilters();
+    });
+    $("#input-title").change(function(){
+        var id = this.id.replace("input","drop");
+        activateBtnClass(id);
+        filter["title"] = this.value;
+        console.log(filter);
+        sendFilters();
+    });
+});
+function activateBtnClass(id){
+        $("#"+id).removeClass("btn-outline-light");
+        $("#"+id).addClass("btn-outline-secondary");
+}
+function deactivateBtnClass(id) {
+        $("#"+id).removeClass("btn-outline-secondary");
+        $("#"+id).addClass("btn-outline-light");
+}
+function sendFilters(){
+        $.ajax({
+            url: "/index",
+            type: "POST",
+            data: JSON.stringify(filter),
+            contentType: "application/json",
+            //complete: callback
+        });
+}
